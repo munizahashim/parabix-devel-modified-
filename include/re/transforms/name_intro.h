43e0527611196d6ff0798444129e0232a17c7dc0
Rob Cameron <cameron@cs.sfu.ca>
2022-07-16 09:33:30 +0000

    Sorting and naming RE alternatives by minimum match length

--- include/re/transforms/name_intro.h
+++ include/re/transforms/name_intro.h
@@ -6,10 +6,14 @@
 
 #pragma once
 
+#include <string>
+
 namespace re {
 class RE;
 
 RE * name_variable_length_CCs(RE * r, unsigned UTF_bits = 8);
+
+RE * name_min_length_alts(RE * r, std::string minLengthPfx = "minLgth");
 
 }
 
